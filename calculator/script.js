a412c29052eac3c41857507db0e91a86bb97b27c
Spiderites <107413455+Spiderites@users.noreply.github.com>
2022-07-13 15:51:21 +0800

    Update script.js

--- calculator/script.js
+++ calculator/script.js
@@ -1,19 +1,61 @@
-var one, two;
-function read1() {
-one = parseInt(document.getElementById("a").value);
+let m = 0;
+function a(){
+    document.getElementById('a').innerHTML = m + "+";
+    m+="+";
 }
-function read2() {
-two = parseInt(document.getElementById("b").value);
+function b(){
+    document.getElementById('a').innerHTML = m + "-";
+    m+="-";
 }
-function add(){
- document.getElementById("answer").innerHTML = one + two;
+function c(){
+document.getElementById('a').innerHTML = m + "×";
+    m+="*";
 }
-function sub(){
- document.getElementById("answer").innerHTML = one - two;
+function d(){
+document.getElementById('a').innerHTML = m + "÷";
+    m+="/";
 }
-function mul(){
- document.getElementById("answer").innerHTML = one * two;
+function e(){
+    document.getElementById('a').innerHTML = m + "1";
+    m+="1";
 }
-function div(){
- document.getElementById("answer").innerHTML = one / two;
+function f(){
+    document.getElementById('a').innerHTML = m + "2";
+    m+="2";
+}
+function g(){
+    document.getElementById('a').innerHTML = m + "3";
+    m+="3";
+}
+function h(){
+    document.getElementById('a').innerHTML = m + "4";
+    m+="4";
+}
+function i(){
+    document.getElementById('a').innerHTML = m + "5";
+    m+="5";
+}
+function j(){
+    document.getElementById('a').innerHTML = m + "6";
+    m+="6";
+}
+function k(){
+    document.getElementById('a').innerHTML = m + "7";
+    m+="7";
+}
+function l(){
+    document.getElementById('a').innerHTML = m + "8";
+    m+="8";
+}
+function m1(){
+    document.getElementById('a').innerHTML = m + "9";
+    m+="9";
+}
+function n(){
+    document.getElementById('a').innerHTML = m + "0";
+    m+="0";
+}
+function o(){
+    m = eval(m)
+    document.getElementById('a').innerHTML = m;
 }
